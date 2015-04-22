9b61b8903300edf068ab005f093f10c31d8ac1a1
sarah <lingwall76@gmail.com>
2015-04-22 12:25:30 -0600

    Final

--- conditionalPlacement/AppDelegate.m
+++ conditionalPlacement/AppDelegate.m
@@ -34,18 +34,10 @@
     
     
     
-    int karl;
-    
-    karl = 10;
-    
-    int leonard;
-    
-    leonard = 7;
-    
-    
+    int karl = 10;
+    int leonard = 7;
     
     int karlPlace = karl > 7 ? 1 : 2;
-    
     int leornardPlace = leonard > 7 ? 1 : 2;
     
     
@@ -56,13 +48,13 @@
         
     }
     
-    else if (karlPlace == 1) {
+    else if (karlPlace == 1 && leonard > 1) {
         
         NSLog(@"Karl is placed 1 and Leonard is placed 2");
         
     }
     
-    else if (leornardPlace == 1) {
+    else if (karlPlace > 1 && leornardPlace == 1) {
         
         NSLog(@"Leonard is placed 1 and Karl is placed 2");
         
@@ -71,25 +63,11 @@
     else
         
     {
-        
-        
-        
         NSLog(@"Karl and Leonard are placed 2");
         
         
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     // Override point for customization after application launch.
